c8034ed765fbbef5d841e9cbd17471b2153487f3
Vinci Xu <xsiyuan@gmail.com>
2023-11-28 15:26:27 +0800

    add: cast

---
+++ by-example/04-type/cast.rs
@@ -0,0 +1,74 @@
+// 不显示类型转换产生的溢出警告。
+#![allow(overflowing_literals)]
+
+fn main() {
+    let decimal = 65.4321_f32;
+
+    // 错误！不提供隐式转换
+    let integer: u8 = decimal;
+    // 改正 ^ 注释掉这一行
+
+    // 可以显式转换
+    let integer = decimal as u8;
+    let character = integer as char;
+
+    println!("Casting: {} -> {} -> {}", decimal, integer, character);
+
+    // 当把任何类型转换为无符号类型 T 时，会不断加上或减去 (std::T::MAX + 1)
+    // 直到值位于新类型 T 的范围内。
+
+    // 1000 已经在 u16 的范围内
+    println!("1000 as a u16 is: {}", 1000 as u16);
+
+    // 1000 - 256 - 256 - 256 = 232
+    // 事实上的处理方式是：从最低有效位（LSB，least significant bits）开始保留
+    // 8 位，然后剩余位置，直到最高有效位（MSB，most significant bit）都被抛弃。
+    // 译注：MSB 就是二进制的最高位，LSB 就是二进制的最低位，按日常书写习惯就是
+    // 最左边一位和最右边一位。
+    println!("1000 as a u8 is : {}", 1000 as u8);
+    // -1 + 256 = 255
+    println!("  -1 as a u8 is : {}", (-1i8) as u8);
+
+    // 对正数，这就和取模一样。
+    println!("1000 mod 256 is : {}", 1000 % 256);
+
+    // 当转换到有符号类型时，（位操作的）结果就和 “先转换到对应的无符号类型，
+    // 如果 MSB 是 1，则该值为负” 是一样的。
+
+    // 当然如果数值已经在目标类型的范围内，就直接把它放进去。
+    println!(" 128 as a i16 is: {}", 128 as i16);
+    // 128 转成 u8 还是 128，但转到 i8 相当于给 128 取八位的二进制补码，其值是：
+    println!(" 128 as a i8 is : {}", 128 as i8);
+
+    // 重复之前的例子
+    // 1000 as u8 -> 232
+    println!("1000 as a u8 is : {}", 1000 as u8);
+    // 232 的二进制补码是 -24
+    println!(" 232 as a i8 is : {}", 232 as i8);
+    // Since Rust 1.45, the `as` keyword performs a *saturating cast*
+    // when casting from float to int. If the floating point value exceeds
+    // the upper bound or is less than the lower bound, the returned value
+    // will be equal to the bound crossed.
+
+    // 300.0 as u8 is 255
+    println!(" 300.0 as u8 is : {}", 300.0_f32 as u8);
+    // -100.0 as u8 is 0
+    println!("-100.0 as u8 is : {}", -100.0_f32 as u8);
+    // nan as u8 is 0
+    println!("   nan as u8 is : {}", f32::NAN as u8);
+
+    // This behavior incurs a small runtime cost and can be avoided
+    // with unsafe methods, however the results might overflow and
+    // return **unsound values**. Use these methods wisely:
+    unsafe {
+        // 300.0 as u8 is 44
+        println!(" 300.0 as u8 is : {}", 300.0_f32.to_int_unchecked::<u8>());
+        // -100.0 as u8 is 156
+        println!(
+            "-100.0 as u8 is : {}",
+            (-100.0_f32).to_int_unchecked::<u8>()
+        );
+        // nan as u8 is 0
+        println!("   nan as u8 is : {}", f32::NAN.to_int_unchecked::<u8>());
+    }
+}

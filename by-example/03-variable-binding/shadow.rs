df2fb29da0cd91c8c7ba3c2a56499aae16793348
Vinci Xu <xsiyuan@gmail.com>
2023-11-28 15:25:42 +0800

    add: variable binding

---
+++ by-example/03-variable-binding/shadow.rs
@@ -0,0 +1,34 @@
+fn main() {
+    // 此绑定生存于 main 函数中
+    let long_lived_binding = 1;
+
+    // 这是一个代码块，比 main 函数拥有更小的作用域
+    {
+        // 此绑定只存在于本代码块
+        let short_lived_binding = 2;
+
+        println!("inner short: {}", short_lived_binding);
+    }
+    // 代码块结束
+
+    // 报错！`short_lived_binding` 在此作用域上不存在
+    // println!("outer short: {}", short_lived_binding);
+    // 改正 ^ 注释掉这行
+
+    println!("outer long: {}", long_lived_binding);
+
+    let shadowed_binding = 1;
+    {
+        println!("before being shadowed: {}", shadowed_binding);
+
+        // This binding *shadows* the outer one
+        let shadowed_binding = "abc";
+
+        println!("shadowed in inner block: {}", shadowed_binding);
+    }
+    println!("outside inner block: {}", shadowed_binding);
+
+    // This binding *shadows* the previous binding
+    let shadowed_binding = 2;
+    println!("shadowed in outer block: {}", shadowed_binding);
+}
